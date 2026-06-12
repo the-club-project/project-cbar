578c9b662202bf7df2739464e03bc2d79fd6e3d8
DaBloat <kurtrusselvillamor1206@gmail.com>
2026-06-13 01:03:30 +0800

    added get_info for CLIO

--- core/clio.py
+++ core/clio.py
@@ -1,5 +1,14 @@
 import zmq
-import struct
+import json
 
 context = zmq.Context()
 subs = context.socket(zmq.SUB)
+ipc_path = "ipc:///tmp/hypr_bridge.ipc"
+
+subs.connect(ipc_path)
+subs.setsockopt_string(zmq.SUBSCRIBE, "")
+
+def get_info():
+    payload_data = subs.recv()
+    data = json.loads(payload_data.decode('utf-8'))
+    return data
---
+++ pop.py
@@ -0,0 +1,4 @@
+from core import clio
+
+while True:
+    print(clio.get_info())

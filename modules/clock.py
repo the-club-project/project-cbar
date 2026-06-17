4becc023ae65a0604821e33f20d86b8bc84a9d0b
DaBloat <kurtrusselvillamor1206@gmail.com>
2026-06-17 15:19:20 +0800

    Removed Notes in Clock Module

--- modules/clock.py
+++ modules/clock.py
@@ -5,6 +5,7 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GLib
 from core.cmodule import CModule
 from datetime import datetime
+import time
 
 class ClockMod(CModule):
     def __init__(self, root):
@@ -22,11 +23,17 @@ class ClockMod(CModule):
         content = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
         content.get_style_context().add_class("popup-box")
         content.set_size_request(popup_w, popup_h)
-
+        
+        
         now = datetime.now()
         self.true_year = now.year
         self.true_month = now.month - 1
         self.last_valid_day = now.day
+
+        tz_name = time.tzname[time.daylight and time.localtime().tm_isdst > 0]
+        offset_seconds = -time.timezone if not time.localtime().tm_isdst else -time.altzone
+        offset_hours = int(offset_seconds / 3600)
+        formatted_tz = f"{tz_name} (UTC{'+' if offset_hours >= 0 else ''}{offset_hours:02d}:00)"
         
         self.is_rolling_back = False
 
@@ -35,25 +42,17 @@ class ClockMod(CModule):
         self.calendar_month.get_style_context().add_class('calendar-header')
         content.pack_start(self.calendar_month, False, False, 0)
 
+        timezone = Gtk.Label(label=f"{formatted_tz}")
+        timezone.get_style_context().add_class('timezone')
+        timezone.set_halign(Gtk.Align.START)
+        content.pack_start(timezone, True, True, 0)
+
         calendar = Gtk.Calendar()
         calendar.set_property("show-heading", False)
         calendar.get_style_context().add_class('calendar')
         
         calendar.connect("day-selected", self.on_day_selected)
         content.pack_start(calendar, True, True, 0)
-
-        notes_today = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing = 2)
-        notes_today.get_style_context().add_class('notes-box')
-        content.pack_start(notes_today, True, True, 0)
-
-        notes_title = Gtk.Label(label="Notes for Today:")
-        notes_title.get_style_context().add_class('notes-header')
-        notes_title.set_halign(Gtk.Align.START)
-        notes_today.pack_start(notes_title, True, True, 0)
-
-        notes = Gtk.Label(label="Aut inveniam viam aut faciam!")
-        notes.get_style_context().add_class('notes')
-        notes_today.pack_start(notes, True, True, 0)
 
         return content
 
